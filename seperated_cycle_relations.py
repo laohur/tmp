2cbcc1914e0105bd58a32d746ee3084ca9638733
laohur <laohur@users.noreply.github.com>
2019-11-08 23:12:49 +0800

    Update and rename seperated_cycle_relations to seperated_cycle_relations.py

--- seperated_cycle_relations.py
+++ seperated_cycle_relations.py
@@ -11,3 +11,11 @@ centers=[1,2,3,4,5,7,8]
 radiuses=[1,1,2,3,2,2,2]
 output:5
 [1,5],[1,7],[1,8],[2,7],[2,8]
+
+
+solution by Heyi
+
+import bisect
+def query_circle(points):
+   right = sorted([p[1] for p in points])
+   return sum(bisect.bisect_left(right, p[0]) for p in points)
